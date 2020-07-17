6482391ac9a0f3f36c806138a743c7714655d6e1
Chad Yoder <cwyoder13@gmail.com>
2020-07-16 20:04:53 -0500

    first three tests passing

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,22 @@
-// YOUR CODE HERE
+function $ (string) {
+  this.elements = [];
+
+  const firstChar = string[0];
+  const strSlice = string.slice(1);
+  let arr = [];
+
+  if (firstChar === '.') {
+    let classes = document.getElementsByClassName(strSlice);
+    classes = [...classes];
+    this.elements = classes;
+  }
+  else if (firstChar === '#'){
+    let id = document.getElementById(strSlice);
+    this.elements.push(id);
+  } else {
+    let tags = document.getElementsByTagName(string);
+    tags = [...tags];
+    this.elements = tags;
+  }
+}
+
